e4a8713370741a4b5596fb56574235bd008c501b
Abavale <40598047+Abavale@users.noreply.github.com>
2018-09-10 11:40:42 +0100

    portfolio page

---
+++ functions sept 5.py
@@ -0,0 +1,33 @@
+'''import math
+sqr = math.pow(2,3)
+sqr = math.sqrt(25)
+pie = math.pi
+
+print (pie)'''
+
+
+class calculate:
+
+
+
+    
+
+    '''def multiply(num, num1):   #it will ask for the variables to be defined
+        print (num * num1)
+        
+    multiply(2, 5)
+    multiply(12, 15)'''
+
+    
+    '''x = int(input("Give me a number: "))
+    y = int(input("Give me a second number: "))
+    def addition (x, y):
+        print (x + y)
+    addition(x, y)'''
+
+    x = 2 #global var
+    def hy():
+        
+        x = 7 #local var
+        return x
+    hy()
---
+++ project 1.py
@@ -0,0 +1,27 @@
+
+'''def osanebi():
+    name = input("what is your name? ")
+    age = int(input("how old are you? "))
+
+    age100 = ((2017 - age) + 100)
+    print(name + " You will be 100 years old in the year " + str(age100))
+osanebi()'''
+
+'''def evenodd():
+    x = int(input("Please insert a number: "))
+    if x >= 0:
+        print ("YOUR NUMBER IS VALID INPUT")
+        if x % 2 != 0 and x % 4 !=0:
+            print ("Not a multiple of 2 and 4")
+        elif x % 2 == 0:
+            print ("This number is  multiple of 2")
+        elif x % 4 == 0:
+            print ("This number is a multiple of 4")
+        else:
+            print()
+    else:
+        print ("INVALID INPUT")
+evenodd()'''
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print (a)
