6d75d74e787c7fd5e9cab77f2c1cf12e72f7fe72
Abavale <40598047+Abavale@users.noreply.github.com>
2018-09-10 12:54:23 +0100

    Delete conditon 1 elif.py

--- conditon 1 elif.py
+++
@@ -1,9 +0,0 @@
-x= int (input ('input a number '))
-if (x>=2 and x<5):
-               print ('cxddgf')
-elif (x==2 ):
-    print('hy')
-elif (x==3):
-               print ('hello')
-else:
-               print ('no conditin met')
